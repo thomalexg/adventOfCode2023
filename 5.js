73d6d3554946113cc870248cc8a21ef3beea9a0f
Thomas Gaedtke <thomalex@thomass-macbook.home>
2023-12-17 15:00:29 +0100

    5 part 1 done

--- 5.js
+++ 5.js
@@ -7,31 +7,28 @@ const seeds = splitted.shift().split(":")[1].trim().split(" ");
 const parts = splitted
 // console.log("parts: ", parts)
 
-const mapObj = {};
-
-const createMapObject = (schema, index) => {
-// console.log("schema: ", schema);
-// console.log("index: ", index)
-if (!mapObj[index]) mapObj[index] = {};
-for (let i = 0; i < Number(schema[2]); i++) {
-    mapObj[index][Number(schema[1])+i] = Number(schema[0])+i;
+const getMappedValue = (source, part) => {
+    let maps = part.split('\n').slice(1);
+    // console.log("map: ", maps);
+    let number;
+    for (let i = 0; i < maps.length; i++) {
+        const mapArr = maps[i].split(" ").map(Number);
+        // console.log("mapArr: ", mapArr);
+        if (source === mapArr[1]) {
+            number = mapArr[0];
+            break;
+        } else if (source > mapArr[1] && source <= (mapArr[1] + mapArr[2])) {
+            let diff = source - mapArr[1];
+            number = mapArr[0] + diff;
+            break; 
+        }
+    }
+    return number !== undefined ? number : source;
 }
-}
 
-const convertFunc = (maps) => {
-    // console.log("maps: ", maps)
-    maps.forEach((map, index) => {
-       let schemas = map.split('\n');
-       let name = schemas.shift();
-    //    console.log("name", name)
-    //    console.log("schemas: ", schemas)
-       schemas.forEach((schema) => {
-        createMapObject(schema.split(" "), index)
-       })
+// Example usage:
+// let result = getMappedValue(sourceValue, partString);
 
-    })
-    //    console.log("mapObj", mapObj)
-}
 
 const getLowestLocationNumber = () => {
     // console.log("seeds: ", seeds);
@@ -39,13 +36,9 @@ const getLowestLocationNumber = () => {
 
     seeds.forEach(seed => {
         let number = Number(seed);
-        Object.keys(mapObj).forEach((key) => {
-            if (mapObj[key][number]) {
-                number = Number(mapObj[key][number]);
-            }
-        });
-        // console.log("seed", seed);
-        // console.log("loop over", number);
+        for (let i = 0; i < parts.length; i++) {
+            number = getMappedValue(number, parts[i])
+        }
         numArr.push(number);
     });
 
@@ -54,6 +47,5 @@ const getLowestLocationNumber = () => {
     console.log("smallestNumber", smallestNumber);
 }
 
-convertFunc(parts)
 getLowestLocationNumber()
 
